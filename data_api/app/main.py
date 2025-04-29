f09ae2091d13947232f0c6efb1989251ddadb9e1
Rodrigo <razevedo.contato@gmail.com>
2025-04-28 21:29:02 -0300

    feat: Create global variable to stores input and implement post endpoint

--- data_api/app/main.py
+++ data_api/app/main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+from typing import List
 
-from app.schemas.schemas import UsersList
+from fastapi import FastAPI
+from pydantic import TypeAdapter
+
+from app.schemas.schemas import User, UsersList
 
 app = FastAPI()
 
+database = dict()
 
-@app.post("/users", response_model=UsersList)
-def post_users():
-    pass
+
+@app.post("/users", status_code=HTTPStatus.CREATED, response_model=UsersList)
+def post_users(users: List[User]):
+    ta = TypeAdapter(List[User])
+    database["users"] = ta.validate_python(users)
+
+    return database
 
 
 @app.get("/superusers")
